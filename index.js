e6bffeb37cafb8dd588107f2110c576bdb3eb07a
Emanuel <emanuel@projectcodex.co>
2016-02-05 18:09:35 +0200

    Create sql database file and routes.

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ var expressHandlebars = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
 
+var connection = require('./connection.js');
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+ 
+  console.log('The solution is: ', rows[0].solution);
+});
+ 
+connection.end();
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
@@ -30,5 +42,5 @@ app.get('/hunce', function(req, res){
 
 var port = process.env.port || 3007;
 app.listen(port, function(){
-    console.log('running at port :' , port)
+    console.log('running at port :' , port);
 });
